532792fcfa6711b59119bd210e2bb13df0d578b3
ChillerDragon <ChillerDragon@gmail.com>
2020-04-23 14:57:32 +0200

    lol I think no design looked better :D

--- index.php
+++ index.php
@@ -7,23 +7,25 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="wrapper">
-        <div class="content">
-            <h1>cstd web paste service</h1>
-            <span>
-                Insert text into text box and click submit!<br>
-                This will generate a paste and give you a unique url to it.<br>
-                <br>
-                THERE IS NO WAY TO DELETE THE PASTE!<br>
-                AND THERE IS ALSO NO GURANTEE PASTES WILL STAY UP FOR EVER!<br>
-                <br>
-                on questions send me a mail at chillerdragon@gmail.com
-            </span>
-            <form action="index.php" method="post">
-                <textarea name="copy-buf" cols="120" rows="20"></textarea>
-                <br>
-                <input type="submit" value="Submit">
-            </form>
+    <div class="waf">
+        <div class="wrapper">
+            <div class="content">
+                <h1>cstd web paste service</h1>
+                <span>
+                    Insert text into text box and click submit!<br>
+                    This will generate a paste and give you a unique url to it.<br>
+                    <br>
+                    THERE IS NO WAY TO DELETE THE PASTE!<br>
+                    AND THERE IS ALSO NO GURANTEE PASTES WILL STAY UP FOR EVER!<br>
+                    <br>
+                    on questions send me a mail at chillerdragon@gmail.com
+                </span>
+                <form action="index.php" method="post">
+                    <textarea name="copy-buf" cols="120" rows="20"></textarea>
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
+                <div class="urls">
 <?php
                 if (isset($_POST['copy-buf']))
                 {
@@ -31,10 +33,18 @@
                     fwrite($buffile, $_POST['copy-buf']);
                     fclose($buffile);
                     $url = shell_exec('/usr/local/bin/cstd buffer.txt');
-                    echo "<br><a href=\"$url\" target=\"_blank\">$url</a><br>";
+                    echo "<br><a class=\"url\" href=\"$url\" target=\"_blank\">$url</a><br>";
                 }
 ?>
+                </div> <!-- urls -->
+            </div>
         </div>
-    </div>
+        <footer>
+            <span>
+                made by ChillerDragon for public and free use<br>
+                code for cstd-web can be found on <strong><a href="https://github.com/ChillerDragon/cstd-web">github</a>.</strong>
+            </span>
+        </footer>
+    </div> <!-- waf -->
 </body>
 </html>
